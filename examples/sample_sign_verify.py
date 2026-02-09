98f0fc0fa7899e45344838dc6dc5128c4d3f8c58
Katulos <katulos@protonmail.com>
2026-02-09 13:46:38 +0000

    refactoring: refactoring samples
    replace pritn/logging

--- examples/sample_sign_verify.py
+++ examples/sample_sign_verify.py
@@ -1,10 +1,17 @@
+import logging
+
 import pycades
 
+logging.basicConfig(level=logging.INFO)
+
 store = pycades.Store()
-store.Open(pycades.CADESCOM_CONTAINER_STORE, pycades.CAPICOM_MY_STORE,
-           pycades.CAPICOM_STORE_OPEN_MAXIMUM_ALLOWED)
+store.Open(
+    pycades.CADESCOM_CONTAINER_STORE,
+    pycades.CAPICOM_MY_STORE,
+    pycades.CAPICOM_STORE_OPEN_MAXIMUM_ALLOWED,
+)
 certs = store.Certificates
-assert(certs.Count != 0), "Certificates with private key not found"
+assert certs.Count != 0, "Certificates with private key not found"
 
 signer = pycades.Signer()
 signer.Certificate = certs.Item(1)
@@ -13,10 +20,10 @@ signer.CheckCertificate = True
 signedData = pycades.SignedData()
 signedData.Content = "Test content to be signed"
 signature = signedData.SignCades(signer, pycades.CADESCOM_CADES_BES)
-print("--Signature--")
-print(signature)
-print("----")
+logging.info("--Signature--")
+logging.info(signature)
+logging.info("----")
 
 _signedData = pycades.SignedData()
 _signedData.VerifyCades(signature, pycades.CADESCOM_CADES_BES)
-print("Verified successfully")
+logging.info("Verified successfully")
